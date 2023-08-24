b9dd1a5919f37911e4c4ab6f0fc6ce9a56e5c64b
Insha Bano <inshabano88158@gmail.com>
2023-08-24 12:25:19 +0530

    Floor

---
+++ src/models/Operator.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Operator {
+    Long EmployeeId;
+    String name;
+
+    public Long getEmployeeId() {
+        return EmployeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        EmployeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/models/ParkingFloor.java
@@ -0,0 +1,46 @@
+package models;
+
+import java.util.List;
+
+public class ParkingFloor extends BaseModel {
+
+    private List<Slot> slots;
+    private int floorNo;
+
+    private ParkingSlotStatus parkingSlotStatus;
+    private ParkingFloor parkingFloor;
+
+
+
+    public List<Slot> getSlots() {
+        return slots;
+    }
+
+    public void setSlots(List<Slot> slots) {
+        this.slots = slots;
+    }
+
+    public int getFloorNo() {
+        return floorNo;
+    }
+
+    public void setFloorNo(int floorNo) {
+        this.floorNo = floorNo;
+    }
+
+    public ParkingSlotStatus getParkingSlotStatus() {
+        return parkingSlotStatus;
+    }
+
+    public void setParkingSlotStatus(ParkingSlotStatus parkingSlotStatus) {
+        this.parkingSlotStatus = parkingSlotStatus;
+    }
+
+    public ParkingFloor getParkingFloor() {
+        return parkingFloor;
+    }
+
+    public void setParkingFloor(ParkingFloor parkingFloor) {
+        this.parkingFloor = parkingFloor;
+    }
+}
